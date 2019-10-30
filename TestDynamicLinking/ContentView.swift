2cbb5cbaccee1d39bc6ad87baef69ff9be365290
Mattia Campolese <info@matsoftware.net>
2019-10-30 14:08:22 +0530

    Initial Commit

---
+++ TestDynamicLinking/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestDynamicLinking
+//
+//  Created by Mattia Campolese on 30/10/2019.
+//  Copyright © 2019 Mattia Campolese. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
